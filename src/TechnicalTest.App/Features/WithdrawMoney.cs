a2293a155062cd1b00e78ed9c8bb2e14b5e2976c
Pete Field <pete.field@gmail.com>
2018-10-05 13:06:24 +0100

    Initial Commit

---
+++ src/TechnicalTest.App/Features/WithdrawMoney.cs
@@ -0,0 +1,23 @@
+﻿using TechnicalTest.App.DataAccess;
+using TechnicalTest.App.Domain.Services;
+using System;
+
+namespace TechnicalTest.App.Features
+{
+    public class WithdrawMoney
+    {
+        private IAccountRepository accountRepository;
+        private INotificationService notificationService;
+
+        public WithdrawMoney(IAccountRepository accountRepository, INotificationService notificationService)
+        {
+            this.accountRepository = accountRepository;
+            this.notificationService = notificationService;
+        }
+
+        public void Execute(Guid fromAccountId, decimal amount)
+        {
+            // TODO:
+        }
+    }
+}
